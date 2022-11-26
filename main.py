39749fa380b7ef5d40469c707221cd0b6bc750f4
Fayroz <hndrfay45@gmail.com>
2022-11-26 21:37:55 +0300

    cw2 done

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 # write your code here
+my_name = input("Whats your name ?")
+my_age = input("Whats old are you ?")
+print(f"My name is {my_name} and I am {my_age} years old")
+
+first_number = 7
+second_number = 4
+operation = input("operation")
+if operation == "+":
+ print(first_number+second_number)
+elif operation == "-":
+  print(first_number-second_number)
+elif operation == "*":
+ print(first_number*second_number)
+elif operation == "/":
+ print(first_number*second_number)
+else :print(" the operation is not valid ")
+
+number = input("Enter a number from 1 to 12")
+noun = input("Enter a noun (plural)")
+name = ("Enter a name")
+sentence = ("Enter any sentence")
+verb = ("Enter a verb")
+print(f"It was {number} o'clock when I heard a knock at the door.I opened the door and there was a box full of {noun} with a note saying From Mr. {name}.Just as I closed the door I heard a scream {sentence}I froze in place and all I could do was {verb}.")
